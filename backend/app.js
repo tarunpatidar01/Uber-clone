f97d5814ea0d097cf7f29680eb8fd81cc6949ada
tarunpatidar01 <tarunpatidar09427@gmail.com>
2024-12-23 22:55:04 +0530

    login complete

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,10 @@ const userRoutes = require('./routes/user.routes')
 connectToDb();
 
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
-app.get('/', (req,res)=>{
+app.get('/', (req ,res)=>{
     res.send("Hello World");
 });
 
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,12 +4,12 @@ const { validationResult } = require('express-validator');
 
 module.exports.registerUser = async (req,res,next) => {
     const errors = validationResult(req);
+    // console.log(req.body);
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array() })
     }
     
     const {fullname, email, password} = req.body;
-
     const hashPassword  = await userModel.hashPassword(password);
 
     const user = await userService.createUser({
@@ -22,4 +22,29 @@ module.exports.registerUser = async (req,res,next) => {
     const token = user.generateAuthToken();
 
     res.status(201).json({ token,user})
+}
+
+module.exports.loginUser = async (req,res,next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({ email }).select('+password')
+
+    if(!user){
+        return res.status(401).json({ message: 'Invalid email or password'});
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if(!isMatch){
+        return res.status(401).json({ message: 'Invalid email or password'});
+    }
+
+    const token = user.generateAuthToken();
+
+    res.status(200).json({ token, user});
 }
